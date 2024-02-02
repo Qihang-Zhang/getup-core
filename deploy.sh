ca13516ef9899020b6df44da7eed8782a548520d
Qihang Zhang <qihangz.work@gmail.com>
2024-02-01 21:27:26 -0800

    2024-02-01 update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ mkdocs gh-deploy --force
 git add .
 git commit -m "$(date +%Y-%m-%d) update"
 git push
-
+echo "===================================="
 echo "Congratulations! Have a nice day!"
+echo "===================================="
 
