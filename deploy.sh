87bd3c29e839005e6d8e49c282bdb4d5ee0d2a6c
Qihang Zhang <qihangz.work@gmail.com>
2024-02-07 22:27:57 -0800

    update readme

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ mkdocs gh-deploy --force
 git add .
 git commit -m "$(date +%Y-%m-%d) update"
 git push
-clear
 
+# below is to print some interesting message, feel free to delete or modify it
+clear
 echo "==========================================================================="
 echo "Morning! Have a nice day!"
 echo "==========================================================================="
